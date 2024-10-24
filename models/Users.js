8b140f5ae79a361c6fa09f538c4ec1ab11475ddd
Alexandrbig1 <alexsmagin1@gmail.com>
2024-10-24 15:12:26 -0500

    feat: add schema, add models

---
+++ models/Users.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+import { handleSaveError, addUpdateSettings } from "./hooks.js";
+import emailRegex from "../regex/emailRegex.js";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      match: emailRegex,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    token: {
+      type: String,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", addUpdateSettings);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const User = model("user", userSchema);
+
+export default User;
