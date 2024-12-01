f31eda4f88eab2cc9e5c7fbe928c38f36dc46c16
DripTrace <rpalm@driptrace.io>
2024-12-01 13:03:18 -0600

    fixing cors

--- app.js
+++ app.js
@@ -13,9 +13,29 @@ const swaggerDocument = swaggerJsdoc(swaggerOptions);
 
 const app = express();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+    ? process.env.ALLOWED_ORIGINS.split(',')
+    : ['https://stately-truffle-e88eb1.netlify.app', 'http://localhost:5173'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+    windowMs: 15 * 60 * 1000,
+    max: 100,
 });
 
 app.use(limiter);
@@ -32,15 +52,15 @@ app.use("/api/users", userRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Page not found" });
+    res.status(404).json({ message: "Page not found" });
 });
 
 app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).json({ error: err.message });
-  } else {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+    if (err.status) {
+        res.status(err.status).json({ error: err.message });
+    } else {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 export default app;
